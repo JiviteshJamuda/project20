1c8fede555e83ea66b4ca2c7e675a5c96af07cc2
JiviteshJamuda <jivitesh2006@gmail.com>
2020-04-08 10:54:57 +0530

    Change The Background Colour

--- sketch.js
+++ sketch.js
@@ -14,15 +14,19 @@ function setup(){
 
 
 function draw(){
-
+background(r,g-30,b*3);
   // change the value of Red based on the mouse movement about the X axis
+  r = map(mouseX,0,1200,200,300);
   // change the value of Green based on the mouse movement about the X axis
+  g = map(mouseX,300,900,200,300);
   // change the value of Blue based on the mouse movement about the X axis
-
+  b = map(mouseX,600,800,200,300);
   // Use the map() function to do so. 
 
   // Pass the values to the background() function you have learnt previously.
 
   // Create an ellipse that will move around with you mouse about the X axis.
+  fill("white");
+  ellipse(mouseX,200,40,40);
   // Remember to fill the canvas with white paint before creating the ellipse.
 }
